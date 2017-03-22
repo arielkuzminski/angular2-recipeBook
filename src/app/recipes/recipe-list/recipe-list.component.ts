b7bc102618dfdd78ef83d4d7eab5801a51f9303a
Ariel <ariel.kuzminski@gmail.com>
2017-03-22 15:51:36 +0100

    Dropdown directive

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,9 +7,13 @@ import {Recipe} from '../recipe';
   templateUrl: './recipe-list.component.html'
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Dummy', 'Dummy', 'http://www.pi-cube.com/wp-content/uploads/2015/04/team-placeholder.jpg', []),
+    new Recipe('Dummy2', 'Dummy2', 'http://www.pi-cube.com/wp-content/uploads/2015/04/team-placeholder.jpg', []),
+    new Recipe('Dummy3', 'Dummy3', 'http://www.pi-cube.com/wp-content/uploads/2015/04/team-placeholder.jpg', [])
+  ];
   @Output() recipeSelected = new EventEmitter<Recipe>();
-  recipe = new Recipe('Dummy', 'Dummy', 'http://www.pi-cube.com/wp-content/uploads/2015/04/team-placeholder.jpg');
+
   constructor() { }
 
   ngOnInit() {
